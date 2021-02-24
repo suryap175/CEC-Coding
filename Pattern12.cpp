49f72fbbb41cc680de43b478ba87021cc8c78e2d
Surya Pratap <surps175@gmail.com>
2021-02-24 23:06:31 +0530

    Pattern 6 (Upwards)

---
+++ Pattern12.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=5,i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        cout<<" ";
+        for(j=0;j<2*i+1;j++)
+        cout<<"*" ;
+        cout<<"\n";
+    }
+    return 0;
+    }
