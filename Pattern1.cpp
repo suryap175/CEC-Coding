384cb192bf61f49732a051ea390f8cd1cd992cf3
Surya Pratap <surps175@gmail.com>
2021-02-24 22:43:55 +0530

    Pattern 1 (*)

---
+++ Pattern1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=5,i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+        cout<<"*";
+        cout<<"\n";
+    }
+    return 0;
+    }
